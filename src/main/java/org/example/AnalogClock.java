da13609527dee6ff1ff8e8f672ee8536fcefb7ba
YSLennon <ysha0123@naver.com>
2024-11-13 02:06:03 +0900

    analog clock... 왤케어려움 ㅠㅠ

--- src/main/java/org/example/AnalogClock.java
+++ src/main/java/org/example/AnalogClock.java
@@ -4,8 +4,11 @@ public class AnalogClock {
 
     public static void main(String[] args) {
 //        System.out.println(Solution.solution(1,5,5,1,5,6));
-        System.out.println(Solution.solution(0,0,0,23,59,59));
-//        Solution.solution(12,0,0,12,0,30);
+//        System.out.println(Solution.solution(0,0,0,23,59,59));
+//        System.out.println(Solution.solution(12,0,0,12,0,30));
+        System.out.println(Solution.solution(11, 58, 59, 11, 59, 0));
+//        Solution.solution(1,5,5,1,5,6);
+
     }
 
     class Solution {
@@ -13,51 +16,41 @@ public class AnalogClock {
             int answer = 0;
             int prevTime = h1*3600+m1*60+s1;
             int nextTime = h2*3600+m2*60+s2;
-            int duration = nextTime-prevTime;
-            int cycle = duration/60;
-
-            answer = cycle*2;
-            answer -= cycle/60;
-            answer += compareLocation(prevTime, nextTime);
-//            boolean checkHS = ((double)prevTime % (3600*12) > (double) prevTime % 60) != ((double)nextTime % (3600*12) > (double) nextTime % 60);
-//            boolean checkMS = ((double)prevTime % (3600) > (double) prevTime % 60) != ((double)nextTime % (3600) > (double) nextTime % 60);
-//            System.out.println(checkHS);
-//            System.out.println(checkMS);
-
-//            if(checkHS){
-//                answer += 1;
-//            }
-//            if(checkMS){
-//                answer += 1;
-//            }
-
-
+            int a = 12*3600;
+//            System.out.println(compareLocation(2*a-1,2*a));
+            for(int i = prevTime ; i < nextTime ; i++){
+                answer += compareLocation(i, i+1);
+            }
 
             return answer;
         }
         static double locationH(int time){
-            return (double) time % (3600*12);
+            return (double) time/120 % 360;
         }
         static double locationM(int time){
-            return (double) time % 3600;
+            return (double) time/10 % 360;
         }
         static double locationS(int time){
-            return (double) time % 60;
+            return (double) time*6 % 360;
         }
         static int compareLocation(int prevTime, int nextTime){
             int value = 0;
-            if((locationH(prevTime) > locationS(prevTime)) != (locationH(nextTime) > locationS(nextTime))){
+            if (locationH(prevTime) == locationS(prevTime) && locationM(prevTime) == locationS(prevTime)){
+                return 1;
+            }
+
+            if((locationH(prevTime) > locationS(prevTime)) && (locationH(nextTime) < locationS(nextTime))){
+                value += 1;
+            } else if((locationH(prevTime) > locationS(prevTime)) && (locationH(nextTime) == locationS(nextTime))){
                 value += 1;
             } else if (locationH(prevTime) == locationS(prevTime)){
                 value += 1;
-            } else if (locationH(nextTime) == locationS(nextTime)){
-                value += 1;
             }
-            if((locationM(prevTime) > locationS(prevTime)) != (locationM(nextTime) > locationS(nextTime))){
+            if((locationM(prevTime) > locationS(prevTime)) && (locationM(nextTime) < locationS(nextTime))){
                 value += 1;
-            } else if (locationM(prevTime) == locationS(prevTime) && locationS(prevTime) != locationH(prevTime)){
+            } else if((locationM(prevTime) > locationS(prevTime)) && (locationM(nextTime) == locationS(nextTime))){
                 value += 1;
-            } else if (locationM(nextTime) == locationS(nextTime) && locationS(nextTime) != locationH(nextTime)){
+            } else if (locationM(prevTime) == locationS(prevTime)){
                 value += 1;
             }
 
